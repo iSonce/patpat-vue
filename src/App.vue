fe7efda3f86a886b7fffb36e7ca1ca3e66ad84d0
iSonce <705213278@qq.com>
2022-06-14 00:44:47 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <post-page></post-page>
+  </div>
+</template>
+
+<script>
+import PostPage from "./pages/PostPage.vue";
+export default {
+  components: { PostPage },
+  name: "App",
+  data() {
+    return {
+      state: "",
+    };
+  },
+  component() {
+    PostPage;
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/pages/PostPage.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="PostPage">
+    <div class="PostHeader">
+      <button id="post-back" @click="ClickBack()">back</button>
+      <span>PatPat</span>
+      <button id="post-more" @click="ClickMore()">more</button>
+    </div>
+    <div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+      <div>HH</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostPage",
+  methods: {
+    ClickBack() {},
+    ClickMore() {},
+  },
+};
+</script>
+<style>
+.PostPage {
+
+  text-align: center;
+}
+.PostHeader {
+  width: 100%;
+  background-color: white;
+  position: fixed;
+  display: flex;
+  box-shadow: 0 -10px #fff;
+}
+</style>
