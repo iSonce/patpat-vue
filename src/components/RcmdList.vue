b14e3a0abc3cfde7ebb03243f7e865dae53b3c8b
iSonce <705213278@qq.com>
2022-06-14 18:35:53 +0800

    adjust style and add func

--- src/components/RcmdList.vue
+++ src/components/RcmdList.vue
@@ -1,23 +1,23 @@
 <template>
-  <a
-    class="rcmd-cards"
-    href="http://www.baidu.com"
-    :key="gameInfo.id"
-    v-for="gameInfo in gameInfos"
-  >
-    <article class="rcmd-card">
+  <a class="rcmd-cards">
+    <a
+      class="rcmd-card"
+      href="www.baidu.com"
+      :key="gameInfo.id"
+      v-for="gameInfo in gameInfos"
+    >
       <img src="../assets/mario.jpg" alt="Game Poster" id="poster" />
       <div class="content">
         <div class="text">
-          <h1 id="name">{{gameInfo.name}}</h1>
-          <p id="discribe">{{gameInfo.discribe}}</p>
+          <h1 id="name">{{ gameInfo.name }}</h1>
+          <p id="discribe">{{ gameInfo.discribe }}</p>
         </div>
         <div class="score">
           <img src="../assets/heart.png" alt="heart" id="heart" />
-          <div id="num">{{gameInfo.score}}</div>
+          <div id="num">{{ gameInfo.score }}</div>
         </div>
       </div>
-    </article>
+    </a>
   </a>
 </template>
 
@@ -30,8 +30,16 @@ export default {
         { id: 1, name: "super mario", discribe: "good game", score: "9.0" },
         { id: 1, name: "super mario", discribe: "good game", score: "9.0" },
         { id: 1, name: "super mario", discribe: "good game", score: "9.0" },
+        { id: 1, name: "super mario", discribe: "good game", score: "9.0" },
+        { id: 1, name: "super mario", discribe: "good game", score: "9.0" },
       ],
     };
+  },
+  methods: {
+    updatedInfos(json) {
+      //用于更新gameInfos的函数
+      this.gameInfos = JSON.parse(json).gameInfos;
+    },
   },
 };
 </script>
@@ -41,11 +49,13 @@ export default {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
   grid-gap: 20px;
-  text-decoration: none;
+  padding: 5px;
+  padding-block-end: 15px;
 }
 
 .rcmd-card {
-  margin-bottom: 20px;
+  -webkit-tap-highlight-color: transparent;
+  text-decoration: none;
   display: grid;
   grid-template-rows: max-content 100px 1fr;
   border-radius: 25px 25px;
@@ -66,7 +76,7 @@ export default {
 }
 
 .rcmd-card .content .text #name {
-  margin: 15px 0px 0px 0px;
+  margin: 10px 0px 0px 0px;
   color: black;
 }
 
